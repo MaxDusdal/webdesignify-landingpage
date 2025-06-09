07d5421512ec00cc00d3730cc203ef4219d72276
Maximilian Tim Dusdal <max@dusdal.de>
2025-06-09 17:36:32 +0200

    FEAT: ci/cd fixes

--- src/app/(front-end)/page.tsx
+++ src/app/(front-end)/page.tsx
@@ -2,7 +2,8 @@ import Blog from '@/components/sections/blog';
 import FAQSection from '@/components/sections/faq-section';
 import { Metadata } from 'next';
 import Projects from '@/components/sections/projects';
-import Hero from '@/components/sections/hero';
+import Differentiators from '@/components/sections/differentiators';
+import SocialProof from '@/components/sections/social-proof';
 import Services from '@/components/sections/services';
 
 export const metadata: Metadata = {
@@ -13,8 +14,9 @@ export const metadata: Metadata = {
 export default function Home() {
     return (
         <>
-            <Hero />
+            <SocialProof />
             <Services />
+            <Differentiators />
             <Projects />
             <FAQSection />
             <Blog />
