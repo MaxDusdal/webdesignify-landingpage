160aad4a20301315b681ee3ae271687d9c42d8a7
Maximilian Tim Dusdal <max@dusdal.de>
2025-06-11 23:40:29 +0200

    FIX: image domain

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
         protocol: "http",
       },
       {
-        hostname: "webdesignify.de",
+        hostname: "www.webdesignify.de",
         protocol: "https",
       },
     ],
